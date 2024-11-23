be3016ae55592e4e834352de3184a6b15bd924e7
Aïcha BOUCHAIKH <aicha.bouchaikh@gmail.com>
2024-11-24 00:27:25 +0100

    "add cors for url conflict

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const { register, login } = require('../src/infrastructure/controllers/UserController')
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3001', // Autorise uniquement cette origine
+}));
 app.use(express.json());
 
+app.get('/register', (req, res) => {
+    res.send('Cette route est prévue pour gérer les inscriptions via une requête POST.');
+});
 app.post('/api/register', register);
 app.post('/api/login', login);
 
